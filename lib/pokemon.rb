dbb4f0cab99dd5cbb0135bf33b1a196317368e25
Benjamin Jones <bljones36@gmail.com>
2018-11-03 15:41:36 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,7 +13,8 @@ class Pokemon
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES ('#{name}', '#{type}')")
-    self.new(name: name, type: type, db: db, id: nil)
+    new_id = db.execute(SELECT id FROM pokemon ORDER BY id DESC LIMIT 1)
+    self.new(name: name, type: type, db: db, id: new_id)
   end
 
   def self.find(id, db)
